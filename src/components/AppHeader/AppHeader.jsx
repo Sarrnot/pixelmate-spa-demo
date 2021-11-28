1e782537f4c7a70a0da8915dc79c3a6edda3a5a3
Sarrnot <urassus@gmail.com>
2021-11-28 19:39:44 +0100

    add AppHeader content and styles

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,7 +1,16 @@
 import styles from "./AppHeader.module.scss";
+import logo from "images/pixelmateLogo/Pixelmate_logo_claim_white.png";
+import NavigationMenu from "components/NavigationMenu/NavigationMenu";
 
 function AppHeader() {
-  return <div className={styles.header}></div>;
+  return (
+    <div className={styles.header}>
+      <a href='/'>
+        <img src={logo} alt='Logo' />
+      </a>
+      <NavigationMenu />
+    </div>
+  );
 }
 
 export default AppHeader;
