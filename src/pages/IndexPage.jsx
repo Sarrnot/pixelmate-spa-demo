6901d1b1e938d133779143a9c63e977279e11469
Sarrnot <urassus@gmail.com>
2021-11-28 19:41:20 +0100

    add IndexPage content

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,3 +1,18 @@
+import { Viewport } from "components/Viewport";
+import { Intro } from "components/content/Intro";
+import { Sponsors } from "components/content/Sponsors";
+import { MeetPixelmate } from "components/content/MeetPixelmate";
+
 export function IndexPage() {
-  return <></>;
+  return (
+    <>
+      <Viewport>
+        <Intro />
+        <Sponsors />
+      </Viewport>
+      <Viewport>
+        <MeetPixelmate />
+      </Viewport>
+    </>
+  );
 }
