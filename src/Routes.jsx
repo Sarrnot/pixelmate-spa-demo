16084fa89ade5c4520f674c2f186c9d68f8a9e6c
Sarrnot <urassus@gmail.com>
2021-11-28 19:37:14 +0100

    add DesignersPage to Routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes as Switch } from "react-router-dom";
 import { IndexPage } from "pages/IndexPage";
+import { DesignersPage } from "pages/DesignersPage";
 import { NotFoundPage } from "pages/NotFoundPage";
 
 export function Routes() {
   return (
     <Switch>
       <Route exact path='/' element={<IndexPage />} />
+      <Route exact path='/designeri' element={<DesignersPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Switch>
   );
