114d6e7711ea471703b82731004ebe6207248c60
Sarrnot <urassus@gmail.com>
2021-11-28 19:40:17 +0100

    add AppFooter content and styles

--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -1,7 +1,14 @@
 import styles from "./AppFooter.module.scss";
+import logo from "images/pixelmateLogo/Pixelmate_logo_claim_white.svg";
 
 function AppFooter() {
-  return <div className={styles.footer}></div>;
+  return (
+    <div className={styles.footer}>
+      <div>© All rights reserved 2013-2019. Pixelmate s.r.o.</div>
+      <img src={logo} alt='Logo' />
+      <div>Zásady ochrany osobních údajů</div>
+    </div>
+  );
 }
 
 export default AppFooter;
