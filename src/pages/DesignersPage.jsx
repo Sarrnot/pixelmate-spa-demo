c901650625d8df855573af621b47169f5f93c080
Sarrnot <urassus@gmail.com>
2021-12-09 10:24:30 +0100

    add designers list loaded from server

--- src/pages/DesignersPage.jsx
+++ src/pages/DesignersPage.jsx
@@ -1,3 +1,44 @@
+import { CardLayout } from "components/Card/CardLayout/";
+import { Card } from "components/Card/Card/";
+import { DesignerPreview } from "components/Designer/DesignerPreview";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export function DesignersPage() {
-  return <></>;
+  const [designersList, setDesignersList] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(({ data }) => {
+        setDesignersList(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError("Něco se pokazilo...");
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <CardLayout>
+      {designersList.map((item) => (
+        <Card key={item.id}>
+          <DesignerPreview
+            name={item.name}
+            username={item.username}
+            email={item.email}
+            address={item.address}
+            website={item.website}
+          />
+        </Card>
+      ))}
+    </CardLayout>
+  );
 }
