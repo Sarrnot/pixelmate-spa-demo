e994c3c44b9c67da282d1b82df8758cf7047c1cc
Sarrnot <urassus@gmail.com>
2021-12-12 21:39:56 +0100

    add button to DesignerPreview and text-align:center website link

--- src/components/Designer/DesignerPreview/DesignerPreview.jsx
+++ src/components/Designer/DesignerPreview/DesignerPreview.jsx
@@ -1,14 +1,17 @@
 import styles from "./DesignerPreview.module.scss";
 import mapIcon from "images/icons/Group 3355.svg";
+import arrowIcon from "images/icons/flaticon1557998900-svg.svg";
 
 function DesignerPreview(props) {
+  const onClickHandler = () => {};
+
   return (
     <>
       <p className={styles.username}>{props.username}</p>
       <p className={styles.name}>{props.name}</p>
       <p className={styles.email}>{props.email}</p>
       <div className={styles.address}>
-        <img src={mapIcon} />
+        <img src={mapIcon} alt='' />
         <div>
           <p className={styles.heading}>{props.address.city}</p>
           <p>{`${props.address.street} ${props.address.suite}`}</p>
@@ -18,6 +21,9 @@ function DesignerPreview(props) {
       <a href='' className={styles.website}>
         {props.website}
       </a>
+      <button onClick={onClickHandler}>
+        Zobrazit profil <img src={arrowIcon} alt='arrowIcon' />
+      </button>
     </>
   );
 }
