626475783f014f135ed527a2ec98802c245e4283
Sarrnot <urassus@gmail.com>
2021-12-21 19:34:27 +0100

    add sidebar navigation menu to NavigationMenu component

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -1,25 +1,44 @@
 import { useState } from "react";
 import styles from "./NavigationMenu.module.scss";
 import { SignInModal } from "components/SignIn/SignInModal";
+import BurgerMenuIcon from "images/icons/burgerMenuIcon.svg";
+import Drawer from "@mui/material/Drawer";
 
 function NavigationMenu(props) {
   const [modalShow, setModalShow] = useState(false);
+  const [sidebarShow, setSidebarShow] = useState(false);
+
+  const handleSidebarClose = () => setSidebarShow(false);
+  const handleSidebarOpen = () => setSidebarShow(true);
 
   const modalOpen = (event) => {
     setModalShow(true);
   };
-
   const modalClose = () => {
     setModalShow(false);
   };
 
-  return (
-    <div className={`${styles.navigationMenu} ${styles[props.theme]}`}>
+  const navMenuOptions = (
+    <>
       <a href='/designeri'>Designéři</a>
       <a href=''>Portfolio</a>
       <button className={styles.signIn} onClick={modalOpen}>
         Přihlásit se
       </button>
+    </>
+  );
+
+  return (
+    <div className={`${styles.navigationMenu} ${styles[props.theme]}`}>
+      <button className={styles.burgerIconButton} onClick={handleSidebarOpen}>
+        <img src={BurgerMenuIcon} alt='burgerMenuIcon' />
+      </button>
+      <Drawer open={sidebarShow} onClose={handleSidebarClose} anchor='right'>
+        <div className={styles.sidebarMenu}>{navMenuOptions}</div>
+      </Drawer>
+
+      <div className={styles.classicMenu}>{navMenuOptions}</div>
+
       <SignInModal modalShow={modalShow} modalClose={modalClose} />
     </div>
   );
